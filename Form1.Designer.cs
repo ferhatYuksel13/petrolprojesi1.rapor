475395809f215f403311511359922cba1d78eb66
ferhatYuksel13 <sehinsah1333@gmail.com>
2024-12-09 18:36:01 +0300

    petrol projesi giriş kısmı
    bütün projeye baştan başlanacaktır uzemden gönderdiğim word belgesinde nedeni yazılı

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace petrolproje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbUserType = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre : ";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUsername.Location = new System.Drawing.Point(125, 40);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 27);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPassword.Location = new System.Drawing.Point(125, 73);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 27);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(125, 140);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(200, 40);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Giriş Yap";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(14, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(311, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Petrol Otomasyon Sistemi Giriş";
+            // 
+            // cmbUserType
+            // 
+            this.cmbUserType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbUserType.FormattingEnabled = true;
+            this.cmbUserType.Items.AddRange(new object[] {
+            "Yönetici",
+            "Personel"});
+            this.cmbUserType.Location = new System.Drawing.Point(125, 106);
+            this.cmbUserType.Name = "cmbUserType";
+            this.cmbUserType.Size = new System.Drawing.Size(200, 28);
+            this.cmbUserType.TabIndex = 6;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(174, 183);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(102, 16);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Şifreyi Unuttum?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kullanıcı Türü : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 203);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.cmbUserType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbUserType;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace petrolproje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Giriş butonuna tıklama olayı
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Kullanıcı adı ve şifreyi alalım
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Kullanıcı türünü seçelim
+            string userType = cmbUserType.SelectedItem.ToString();
+
+            // Veritabanı bağlantı dizesi
+            string connectionString = "Server=LEGEND; Database=PetrolProje; Integrated Security=True;";
+
+            // Kullanıcı doğrulama sorgusu
+            string query = "SELECT COUNT(*) FROM users WHERE username = @username AND password = @password AND userType = @userType";
+
+            // Veritabanı bağlantısını açalım
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Bağlantıyı açalım
+                    connection.Open();
+
+                    // SQL komutunu hazırlayalım
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Parametreleri ekleyelim
+                        command.Parameters.AddWithValue("@username", username);
+                        command.Parameters.AddWithValue("@password", password);
+                        command.Parameters.AddWithValue("@userType", userType);
+
+                        // Sonucu alalım
+                        int result = (int)command.ExecuteScalar();
+
+                        // Giriş başarılı ise
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Giriş başarılı! Hoşgeldiniz, " + userType + ".", "Giriş Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            // Kullanıcı türüne göre yönlendirme yapalım
+                            if (userType == "Yönetici")
+                            {
+                                // Yönetici ekranına yönlendirme
+                                // AdminPanelForm adminPanel = new AdminPanelForm();
+                                // adminPanel.Show();
+                            }
+                            else if (userType == "Personel")
+                            {
+                                // Personel ekranına yönlendirme
+                                // PersonnelDashboardForm personnelForm = new PersonnelDashboardForm();
+                                // personnelForm.Show();
+                            }
+
+                            // Giriş formunu kapat
+                            this.Hide();  // Giriş formunu gizler, ancak form hala açık kalır.
+                        }
+                        else
+                        {
+                            // Giriş başarısız
+                            MessageBox.Show("Kullanıcı adı veya şifre hatalı! Lütfen tekrar deneyin.", "Giriş Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Veritabanı bağlantı hatası
+                    MessageBox.Show("Veritabanı bağlantı hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Şifremi Unuttum linkine tıklama olayı
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Şifre sıfırlama işlemi başlatılabilir
+            MessageBox.Show("Şifre sıfırlama işlemi başlatıldı.");
+            // Veya yeni bir şifre sıfırlama formu açılabilir
+            // ResetPasswordForm resetForm = new ResetPasswordForm();
+            // resetForm.Show();
+        }
+    }
+}
